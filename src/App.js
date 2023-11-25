e0439217f5411e81e92c09fe51494bd3e42c5d92
IgorMontezuma20 <igorminranda17@gmail.com>
2023-11-25 20:17:51 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+let initialState = [
+  {
+    id: 1,
+    descricao: "Estudar React",
+  },
+  {
+    id: 2,
+    descricao: "Estudar React Native",
+  },
+  {
+    id: 3,
+    descricao: "Estudar Node",
+  },
+];
 
 function App() {
+  const [atividades, setAtividades] = useState(initialState);
+
+  function addAtividade(e) {
+    e.preventDefault();
+    const atividade = {
+      id: document.getElementById("id").value,
+      descricao: document.getElementById("descricao").value,
+    };
+
+    console.log(atividades);
+    setAtividades([...atividades, { ...atividade }]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <input id="id" type="text" placeholder="id" />
+        <input id="descricao" type="text" placeholder="descrição" />
+        <button type="submit" onClick={addAtividade}>
+          Salvar
+        </button>
+      </form>
+      <div className="mt-3">
+        <ul className="list-group">
+          {atividades.map((atividade) => (
+            <li className="list-group-item" key={atividade.id}>
+              {atividade.id} - {atividade.descricao}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
